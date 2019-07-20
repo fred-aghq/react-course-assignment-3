bb24afbdc4011b24384d44dd24089d37b4be5136
Fred Spooner <fred@fredspooner.com>
2019-07-20 18:52:24 +0100

    Implements Courses logic and route

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Course from '../Course/Course';
 import './Courses.css';
 
 class Courses extends Component {
     state = {
         courses: [
-            { id: 1, title: 'Angular - The Complete Guide' },
-            { id: 2, title: 'Vue - The Complete Guide' },
-            { id: 3, title: 'PWA - The Complete Guide' }
+            {id: 1, title: 'Angular - The Complete Guide'},
+            {id: 2, title: 'Vue - The Complete Guide'},
+            {id: 3, title: 'PWA - The Complete Guide'}
         ]
-    }
+    };
 
-    render () {
+    loadCourse = course => {
+        const pathname = '/courses/' + course.id;
+        this.props.history.push({pathname: pathname, state: {title: course.title}});
+    };
+
+    render() {
         return (
-            <div>
-                <h1>Amazing Udemy Courses</h1>
-                <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
-                </section>
-            </div>
+                <div>
+                    <h1>Amazing Udemy Courses</h1>
+                    <section className="Courses">
+                        {
+                            this.state.courses.map(course => {
+                                return <article
+                                    onClick={() => this.loadCourse(course)} className="Course" key={course.id}
+                                >{course.title}</article>;
+                            })
+                        }
+                    </section>
+                    <Switch>
+                        <Route path="/courses/:id" component={Course} params={{courseData: this.state.courses}}/>
+                    </Switch>
+                </div>
         );
     }
 }
