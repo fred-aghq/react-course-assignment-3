063a7637f4743fd70f96c2014f7750f37e643b19
Fred Spooner <fred@fredspooner.com>
2019-07-20 18:51:32 +0100

    Course uses react router state to get title, falls back to props

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+        const title = !this.props.history.location.state.title ? this.props.courseData.title : this.props.history.location.state.title;
+
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
